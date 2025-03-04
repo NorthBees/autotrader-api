071286c8c5fbb99c80ad4d4987938686a922b9ed
Liam Thursfield <liam3006x@gmail.com>
2025-03-04 18:53:25 +0000

    test(ISSUE-5): Vehicle Metrics API
    - Add test for Vehicle Metrics Trait

--- tests/Feature/AutoTraderWebServiceVehicleMetricsTrait.php
+++ tests/Feature/AutoTraderWebServiceVehicleMetricsTrait.php
@@ -2,5 +2,43 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Arr;
+use NorthBees\AutoTraderApi\Tests\Traits\TestableAutoTraderVehicleMetricsTrait;
 
-todo("tests!");
+$trait = new TestableAutoTraderVehicleMetricsTrait();
+
+describe('AutoTraderVehicleMetricsTrait', function () use ($trait) {
+    it("formatVehicleMetricOptions will transform features to the expected format", function () use ($trait) {
+        $features = [
+            "one",
+            "two"
+        ];
+
+        $options = ['features' => $features];
+
+        $result = $trait->publicFormatVehicleMetricOptions($options);
+
+
+        $features = $result['features'];
+
+        expect(Arr::get($features, '0.name'))
+            ->toBe('one')
+            ->and(Arr::get($features, '1.name'))
+            ->toBe('two');
+    });
+
+    it("formatVehicleMetricOptions will set the expected key when totalPrice is provided", function () use ($trait) {
+        $price = 123;
+        $options = ['totalPrice' => $price];
+
+        $result = $trait->publicFormatVehicleMetricOptions($options);
+
+
+        expect(Arr::get($result, 'adverts.retailAdverts.price.amountGBP'))
+            ->toBe($price)
+            ->and($result)
+            ->not()
+            ->toHaveKey('totalPrice');
+
+    });
+})->group('autotrader-api', 'vehicle-metrics');
